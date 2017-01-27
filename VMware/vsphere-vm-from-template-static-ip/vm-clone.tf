875d9f471fe0b90254b0d24a27ebe97f477bb543
discoposse <eric@discoposse.com>
2017-01-27 11:48:44 -0500

    changing vsphere samples and removing logs with the gitignore

--- VMware/vsphere-vm-from-template-static-ip/vm-clone.tf
+++ VMware/vsphere-vm-from-template-static-ip/vm-clone.tf
@@ -1,20 +1,3 @@
-resource "vsphere_virtual_machine" "photonOS01" {
-  name   = "photonOS01"
-  vcpu   = 1
-  memory = 2048
-  datacenter = "DiscoMacbook"
-  cluster = "CLUS-01"
-
-  network_interface {
-  	label = "VM Network"
-  }
-
-  disk {
-  	template = "TMPL_photonOS"
-  	type = "thin"
-  }
-}
-
 resource "vsphere_virtual_machine" "photonOS02" {
   name   = "photonOS02"
   vcpu   = 1
@@ -23,8 +6,28 @@ resource "vsphere_virtual_machine" "photonOS02" {
   cluster = "CLUS-01"
 
   network_interface {
+    label = "VM Network"
+    ipv4_address = "172.16.223.162"
+    ipv4_prefix_length = "24"
+    ipv4_gateway = "172.16.223.2"
+  }
+
+  disk {
+    template = "TMPL_photonOS"
+    type = "thin"
+  }
+}
+
+resource "vsphere_virtual_machine" "photonOS03" {
+  name   = "photonOS02"
+  vcpu   = 1
+  memory = 2048
+  datacenter = "DiscoMacbook"
+  cluster = "CLUS-01"
+
+  network_interface {
   	label = "VM Network"
-  	ipv4_address = "172.16.223.161"
+  	ipv4_address = "172.16.223.163"
   	ipv4_prefix_length = "24"
   	ipv4_gateway = "172.16.223.2"
   }
